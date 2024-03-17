a0d3d2f7117fb251ba617e10d42ba18fa0ba2efa
wz <599508997@qq.com>
2024-03-17 23:03:24 +0800

    接口测试

---
+++ src/main/java/com/web/springbootweb/test/TestInterface.java
@@ -0,0 +1,12 @@
+package com.web.springbootweb.test;
+
+/**
+ * @description:
+ * @author: ywz
+ * @time: 2024/3/17 12:54
+ */
+public interface TestInterface {
+
+
+    public  void testMethod();
+}
