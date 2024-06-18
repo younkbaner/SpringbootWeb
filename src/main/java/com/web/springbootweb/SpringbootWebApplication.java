0a8c04e4b23302149ae6c1a2ab7afbda0452e07f
wz <599508997@qq.com>
2024-06-18 13:54:06 +0800

    引入 hutool 工具类

--- src/main/java/com/web/springbootweb/SpringbootWebApplication.java
+++ src/main/java/com/web/springbootweb/SpringbootWebApplication.java
@@ -1,7 +1,6 @@
 package com.web.springbootweb;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication  //告诉SpringBoot 这是一个SpringBoot程序
@@ -10,7 +9,7 @@ public class SpringbootWebApplication {
 
     public static void main(String[] args) {
         //定义一个主类
-        SpringApplication.run(SpringbootWebApplication.class, args);
+
     }
 
 }
