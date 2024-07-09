810e8b3d15873de0c246131098f74abf1d88db44
wz <599508997@qq.com>
2024-07-09 13:18:13 +0800

    list 分割

--- src/main/java/com/web/springbootweb/ApolloKeyListener.java
+++ src/main/java/com/web/springbootweb/ApolloKeyListener.java
@@ -5,10 +5,8 @@ import com.ctrip.framework.apollo.ConfigChangeListener;
 import com.ctrip.framework.apollo.ConfigService;
 import com.ctrip.framework.apollo.model.ConfigChange;
 import com.ctrip.framework.apollo.model.ConfigChangeEvent;
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 /**
  * @description:
@@ -34,7 +32,7 @@ public class ApolloKeyListener {
                 }
             }
         });
-
+        System.out.println("测试一下");
         return new ApolloKeyListener();
     }
 }
